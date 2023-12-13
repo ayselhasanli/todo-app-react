0f26b0bf18918379a188f870128d123bc7b00319
ayselhasanli <aysiiihasan@gmail.com>
2023-12-13 23:37:04 +0400

    template created, add some main functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {Row, Col} from "reactstrap";
+import {FaRegEdit} from "react-icons/fa";
+import {MdDelete} from "react-icons/md";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [active, setActive] = useState(false)
+    const [value, setValue] = useState("")
+
+    const holdInputValue = event =>{
+        setValue(event.target.value)
+        console.log(value)
+    }
+
+    function valueError () {
+        console.log()
+        setActive(!active)
+    }
+
+    return (
+        <div className="App">
+            <div className="todo-card">
+                <div className={active ? "error-inner" : "error-inner not-active"}>
+                    Please enter value
+                </div>
+                <h1>Todo List</h1>
+                <div className="todo-card-input">
+                    <Row>
+                        <Col xs={12} sm={6} md={8} xl={10}>
+                            <input onChange={holdInputValue} placeholder={"I must do it! :)"} type="text"/>
+                        </Col>
+                        <Col xs={12} sm={6} md={4} xl={2}>
+                            <button onClick={valueError} id={"submit"}>Submit</button>
+                        </Col>
+                    </Row>
+                </div>
+                <div className="todos-container">
+                    <div className="todo">
+                        <Row>
+                            <Col xl={10}>
+                                Todo
+                            </Col>
+                            <Col>
+                                <button id={"edit-todo"}>
+                                    <FaRegEdit/>
+                                </button>
+                                <button id={"delete-todo"}>
+                                    <MdDelete/>
+                                </button>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+                <div className="clear-section">
+                    <button id={"clear"}>Clear Items</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
